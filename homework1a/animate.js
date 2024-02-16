3242646a68793e1e5bf9289a2d9f1021fbd2f7ef
MrNitemare <60372949+MrNitemare@users.noreply.github.com>
2024-02-15 23:40:22 -0800

    Add files via upload

--- homework1a/animate.js
+++ homework1a/animate.js
@@ -10,12 +10,12 @@
 */
 $(document).ready(function() {
     $("#animate").click(function() {
-        $("header h1").css("color", "#f37736");
+        $("header h1").css({color: "#f37736"});
         $("main img").css({border: '8px solid #f37736'});
         $("header h1").animate({"font-size": "250%"}, 4000);
         $("header h1").animate({"font-size": "170%"}, 3000);
     	var timer = setTimeout(function() {
-		$("header h1").css("color", "#800000");
+	    $("header h1").css({color: "#800000"});
 	}, 7000); // end setTimeout()
     }); // end click()
 }); // end ready()
